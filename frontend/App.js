036f01c7b56ea4ff0fb8d6fd86e9b2e2f3d13ee5
Wei Xuan <23021779@myrp.edu.sg>
2025-02-05 10:54:29 +0800

    updated AuthContext.js

--- frontend/App.js
+++ frontend/App.js
@@ -2,13 +2,6 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
  
 // pages & components
-<<<<<<< HEAD:frontend/App.js
-import Home from "./src/pages/Home";
-import Login from "./src/pages/Login";
-import Signup from "./src/pages/Signup";
-import AddRecipe from "./pages/AddRecipe";
-import Navbar from "./src/components/Navbar";
-=======
 import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 import Signup from "./Pages/Signup";
@@ -16,7 +9,6 @@ import Navbar from "./components/Navbar";
 import RecipeForm from './components/RecipeForm';
 import RecipeEdit from './components/RecipeEdit';
 import UploadImage from './components/UploadImage';
->>>>>>> 2c135c927c9917948a0283622718c8183c5a9cdb:frontend/src/App.js
 import "./App.css";
  
 function App() {
