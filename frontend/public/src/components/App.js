53252ec3aa96e6957dba066cea377fee26950d86
ChengFangRong <23024324@myrp.edu.sg>
2025-02-05 10:12:18 +0800

    app.js

--- frontend/public/src/components/App.js
+++ frontend/public/src/components/App.js
@@ -1,1 +1,44 @@
+import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+ 
+// pages & components
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import AddRecipe from "./pages/AddRecipe";
+import Navbar from "./components/Navbar";
+import "./App.css";
+ 
+function App() {
+ 
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <div className="pages">
+          <Routes>
+            <Route
+              path="/"
+              element={<Home />}
+            />
+            <Route
+              path="/login"
+              element={<Login />}
+            />
+            <Route
+              path="/signup"
+              element={<Signup />}
+            />
+            <Route
+              path="/add-recipe"
+              element={<AddRecipe />}
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+ 
+export default App;
+ 
