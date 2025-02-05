379f1b7339b2d5f2e153b0ecb8cec3a89f78ecb5
Gideon <23023165@myrp.edu.sg>
2025-02-05 15:28:37 +0800

    updated index and home

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -4,7 +4,7 @@ import { useAuthContext } from "../Hooks/useAuthContext"
 import RecipeForm from "../Components/RecipeForm";
 
 // components
-import RecipeDetails from '../components/RecipeDetails'
+import RecipeDetails from '../Components/RecipeDetails'
 
 
 const Home = () => {
